0b08de6c559caa44566c2de3a8d97ab06ea39a86
Denis Krebs <45010238+deniskrebs@users.noreply.github.com>
2018-11-27 16:06:53 +0100

    Add files via upload

---
+++ 6_1_Centrality.R
@@ -0,0 +1,163 @@
+
+# Set the working directory to the folder containing the workshop files:
+setwd("C:/Users/Denis Krebs/Desktop/Thesis/Analysis")
+
+source("./1_LoadData.R") 
+
+#################### NODE-LEVEL
+#One challenge in studying complex networks is to develop simplified measures that capture some elements of the structure in an understandable way.
+#One such simplification is to ignore any patterns among different nodes, but just look at each node separately.
+
+#################### CENTRALITY
+#centralization: C(G)=sum( max(c(w), w) - c(v),v)
+centr_degree(g.un, normalized = TRUE)$centralization
+centr_degree(igraph::simplify(g.un), normalized = TRUE)$centralization
+
+centr_betw(g.un, nobigint = TRUE, normalized = TRUE)$centralization
+centr_betw(igraph::simplify(g.un), nobigint = TRUE, normalized = TRUE)$centralization
+
+centr_clo(g.un)$centralization # same for g.simp.un
+
+
+
+
+##DEGREE
+
+#a measure as simple as the degree distribution can give us a glimpse into the structure of a network and distinguish different types of networks. 
+#Obviously, the degree distribution captures only a small amount of the network structure, as it ignores how the nodes are connected to each other
+
+
+#NOT SIMPLIFIED - number of mails (not number of persons) - people who sent many mails have high centrality (influence / importance)
+
+#UNDIRECTED - Who communicates a lot via mail? / Who is involved in a lot of mail traffic?
+# degree - how many mails the individual had contact with (either sent or received)
+centr_degree(g.un) #INTERPRETATION?
+degree_all <- data.frame(degree(g.un, loops = FALSE))
+mean(degree(g.un, loops = FALSE)) #involved in 5952 mails on average (10 mails per day)
+quantile(degree(g.un, loops = FALSE)) # 3/4 of the nodes have a degree smaller than 8809 and only 1/4 more (up to 42885 mails in 595 days (72 mails per day))
+density(degree(g.un, loops = FALSE))       #why is density min. -4190 and max. 47077 (4192 to high)?
+plot(density(degree(g.un, loops = FALSE)))    #INTERPRETATION?
+hist(degree(g.un),main="Number of Mails (in & out)",
+     xlab="total degree (g.un)",
+     ylab="probability density",
+     col="deepskyblue",
+     breaks=seq(0,45000,length=(max(degree_all)/1000)), 
+     freq=FALSE)
+m <- mean(degree(g.un), loops = FALSE)
+s <-  sd(degree(g.un), loops = FALSE)
+curve(dnorm(x,m,s),add=TRUE,lwd=3) #quite normal distributed, but many nodes involved in less than 1000 mails and a few hubs (involved in over 20'000 mails)
+box() 
+degree_all[order(degree_all$degree.g.un..loops...FALSE.,decreasing=T)[1:5],] 
+# nodes involved in many mails: 15.09.00.00.00.00.14.052, 10.02.05.02.00.00.08.006, 10.06.00.00.00.00.08.032, 00.01.00.00.00.00.12.004, 15.09.00.00.00.00.08.03
+
+
+#SIMPLIFIED - number of people (not number of mails) - So people with connections to many different people have high centrality (influence / importance)
+
+#UNDIRECTED - Who communicates with a lot of different people? / Who is connected to many?
+# degree - to how many people the individual had contact(either sent or received)
+centr_degree(g.simp.un) #INTERPRETATION?
+degree_simp <- data.frame(degree(g.simp.un, loops = FALSE))
+mean(degree(g.simp.un, loops = FALSE)) #contact to 64 persons on average
+quantile(degree(g.simp.un, loops = FALSE)) # 3/4 of the nodes have contact to less than 82 persons and only 1/4 more (up to 566)
+density(degree(g.simp.un, loops = FALSE))       #why is density min. -25.95 and max. 592.95 (25.95 to high)?
+plot(density(degree(g.simp.un, loops = FALSE)))    #INTERPRETATION?
+hist(degree(g.simp.un),main="Number of People (in & out)",
+     xlab="total degree (g.simp.un)",
+     ylab="probability density",
+     col="deepskyblue",
+     breaks=seq(0,600,length=(max(degree_simp)/10)), 
+     freq=FALSE)
+m <- mean(degree(g.simp.un), loops = FALSE)
+s <- sd(degree(g.simp.un))
+getmode(degree(g.simp.un, loops = FALSE))
+curve(dnorm(x,m,s),add=TRUE,lwd=3) #quite normal distributed, but most nodes have contact to less than 60 people and a few hubs (contact to over 200 people)
+box() 
+#In a real world network, most nodes have a small degree, but a few nodes have very large degree (hubs) - long tail / power law degree distribution.
+#not really the case here (but a bit)
+degree_simp[order(degree_simp$degree.g.simp.un..loops...FALSE.,decreasing=T)[1:5],] 
+# nodes with contact to many people: 00.01.00.00.00.00.15.013, 12.02.00.00.00.00.10.053, 04.02.00.00.00.00.08.030, 00.01.00.00.00.00.02.005, 00.01.00.00.00.00.03.023
+
+
+
+
+
+
+
+
+
+### BETWEENNESS - (simpliefied doesn't matter here)
+# number of shortest paths going through a specific vertex - control of a human on the communication between other humans in a social network
+# removal of individuals with high betweenness will most disrupt communications between other vertices
+# (usually concerned only with the relative magnitudes of the centralities and not with their absolute values. 
+# The sum can be normalized by dividing by the total number of ordered pairs of nodes, which is n2, so that betweenness lies strictly between 0 and 1)
+# Betweenness centrality, as defined above, is a measure of information control assuming two important hypothesis: 
+#(i) every pair of vertices exchange information with equal probability, and 
+#(ii) information flows along the geodesic (shortest) path between two vertices, or one of such path, chosen at random, 
+#if there are several. However, information not always takes the shortest route. 
+#In social network, for instance, a news about a friend of us might not come directly from the friend but from another mutual friend. 
+
+
+# undirected - simplify no influence
+#who often acts as a bridge in communication between other nodes (can you introduce me to that person?)
+betweenness_un <- data.frame(betweenness(g.un)) # max possible value (theoretically): n2???(n???1)=n2???n+1=352'241
+mean(betweenness(g.un))  # on average a node lies on 272 shortest paths
+quantile(betweenness(g.un)) # 3/4 of the nodes are on less than 178 shortest paths and only 1/4 on more (up to 47605)
+density(betweenness(g.un))       #why is density min. -97.37 and max. 47702.91 (97.37 to high)?
+plot(density(betweenness(g.un)))  #INTERPRETATION?
+hist(betweenness(g.un),main="Number of shortest paths",
+     xlab="beteweenness (g.un)",
+     ylab="probability density",
+     col="deepskyblue",
+     breaks=seq(0,50000,length=(max(betweenness_un)/500)), 
+     freq=FALSE)
+m <- mean(betweenness(g.un))
+s <-  sd(betweenness(g.un))
+curve(dnorm(x,m,s),add=TRUE,lwd=3) #quite normal distributed, but most nodes received mails from less than 50 people and a few hubs (received mails from over 150 people)
+box() 
+betweenness_un[order(betweenness_un$betweenness.g.un.,decreasing=T)[1:5],] 
+# nodes that are on most shortest paths: 00.01.00.00.00.00.15.013, 12.02.00.00.00.00.10.053, 04.02.00.00.00.00.08.030, 00.01.00.00.00.00.02.005, 00.01.00.00.00.00.03.023
+
+
+
+
+
+
+
+##Closeness - simplify has no influence 
+#how long it will take to spread information from v to all other nodes sequentially farness/peripherality of a node v 
+#is defined as the sum of its distances to all other nodes mean geodesic distance to all other nodes --> good broadcasters
+#(close to being the mean shortest path, but gives higher values for more central nodes)
+#inverse of the sum of the shortest distances between each node and every other node in the network
+
+# undirected - who often acts as a bridge in communication between other nodes (can you introduce me to that person?)
+closeness_un <- data.frame(closeness(g.un)) 
+mean(closeness(g.un))  #INTERPRETATION?
+quantile(closeness(g.un))  #INTERPRETATION?
+density(closeness(g.un))    #INTERPRETATION?
+plot(density(closeness(g.un)))  #INTERPRETATION?
+hist(closeness(g.un),main="Closeness",
+     xlab="closeness (g.un)",
+     ylab="frequency",
+     col="deepskyblue",
+     freq=TRUE)
+m <- mean(closeness(g.un))
+s <-  sd(closeness(g.un))
+curve(dnorm(x,m,s),add=TRUE,lwd=3)   #INTERPRETATION?
+box() 
+closeness_un[order(closeness_un$closeness.g.un.,decreasing=T)[1:5],] 
+# nodes wich are the closest: 00.01.00.00.00.00.15.013, 12.02.00.00.00.00.10.053, 04.02.00.00.00.00.08.030, 00.01.00.00.00.00.02.005, 00.01.00.00.00.00.03.023
+
+
+
+
+
+
+
+
+
+##compare centrality measures
+centrality <- data.frame(V(g)$name, degree_simp, degree_all, betweenness_un, closeness_un)
+centrality_correlation <- as.matrix(cor(centrality[,2:5]))
+
+compare(centrality$degree.g.simp.un..loops...FALSE., centrality$degree.g.un..loops...FALSE., method = "nmi")
+# why not the same number as in correlation table?
---
+++ 6_1_NodeLevel.R
@@ -0,0 +1,81 @@
+
+# Set the working directory to the folder containing the workshop files:
+setwd("C:/Users/Denis Krebs/Desktop/Thesis/Analysis")
+
+source("./1_LoadData.R") 
+
+
+################## NODE LEVEL - DISCRIPTIVE
+
+################## CONNECTED COMPONENTS
+components(g, mode = "strong")  # 578 nodes (97%) are strongly connected (respect the direction)
+components(g, mode = "weak")  # All nodes are weakly connected (disregarding the direction)
+################## REACHABILITY
+#reach <- reachability(g)
+igraph::is.connected(g.un, mode = "strong")
+igraph::is.connected(g, mode = "strong")
+
+
+################## RECIPROCITY = MUTUALITY
+#likelihood of vertices in a directed network to be mutually linked
+#estimation the error introduced when a directed network is treated as undirected 
+#probability that the opposite counterpart of a directed edge is also included in the graph
+reciprocity(g, ignore.loops = TRUE)  # if one person sends a mail to another, the chances are 76% that this other person also sent a mail to him/her
+reciprocity(g.simp, ignore.loops = TRUE)  # why not the same as for g? (Simplify removed multiple edges, but doesn't it leave one edge remaining)
+#mutual links facilitate the transportation process
+#do mutual links occur more or less often than expected by chance
+# --> as expected: most connections are reciprocal
+
+
+
+
+# FROM HERE EVERYTHING WITH UNDIRECTED:
+# 1. Louvain only with undirected network
+# 2. Reciprocity high (0.76) --> don't loose much information
+# 3. Mainly interessted in who has contact with who and not who sends / receives mails to / from who
+
+
+
+
+################## TRANSITIVITY = CLUSTERING COEFFICIENT
+# probability that adjacent vertices are connected - Probability that a friend of my friend is a friend of mine as well
+transitivity(g.un) #global: 38.6% chance (high) that people who share a node who they are communicated with have also communicated directly with each other
+transitivity(g.un, type = "average") #average of local: 0.584 (WHY DIFFERENT FROM GLOBAL?)
+
+
+################## DENSITY
+# portion of potential connections
+graph.density(g.un)  # 11.3% of the possible activity takes place if not accounting for direction
+graph.density(g.simp.un)  #Why not the same as g.un?
+
+
+################## ASSORTATIVITY
+# do high degree nodes mostly interact with other high degree nodes
+#correlation between nodes of similar degree, while negative values indicate relationships between nodes of different degree
+assortativity(g.un, types1 = nodes$ID)  # people who are touching a lot of mails tend to interact with people also touching a lot of mails
+assortativity(g.simp.un, types1 = nodes$ID)  # people who have conntact with many people tend to have contact with people who know a lot of people
+#assortativity(g.simp, types1 = nodes$ID , directed = FALSE)  #why not the same as g.simp.un?
+
+
+################## PATHLENGTH
+###average path length = mean distance
+average.path.length(g.un)  # on average you need two nodes to reach any other person from the network (2 (not 6 like worldwide) degrees of separation)
+
+###Distance Histogram
+paths_undir = distance_table(g.un)$res
+names(paths_undir) = 1:length(paths_undir)
+barplot(paths_undir / sum(paths_undir), xlab="Distance (undirected)", ylab="Frequency")
+###Diameter: 
+#longest path between two nodes (size of network)
+diameter(g.un)  # the two nodes the farest appart are separated by 4 nodes
+#which two edges are the farest apart
+get_diameter(g.un, weights=NA)
+# highlight the diameter
+d = get_diameter(g.simp.un)
+E(g.simp.un)$color = "grey"
+E(g.simp.un)$width = 1
+E(g.simp.un, path=d)$color = "red"
+E(g.simp.un, path=d)$width = 2
+V(g.simp.un)$color  = "white"
+V(g.simp.un)[d]$color = "red"
+plot(g.simp.un, vertex.label = NA, vertex.size=1, edge.curved=.25, edge.arrow.size=0.01)
